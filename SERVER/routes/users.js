687279b9de32936ec335d2f5e1e1c59f0dc5b7a0
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-04-17 02:36:26 +0100

    ch(test):test coverage increase test coverage fix server not starting implement feedback to  move routes to index
    [Finishes #165329525]

--- SERVER/routes/users.js
+++ SERVER/routes/users.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import userControllers from '../controllers/userControllers';
+import UserController from '../controllers/userControllers';
 import InputValidator from '../middlewares/inputValidation';
 
 const router = express.Router();
 router.use(bodyParser.json());
 
-// router.post('/signup', InputValidator.signup, UserController.signUp);
-router.post('/signin', InputValidator.signin, userControllers.signIn);
+router.post('/signup', InputValidator.signup, UserController.signUp);
+router.post('/signin', InputValidator.signin, UserController.signIn);
 
 export default router;
