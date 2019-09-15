a0a1dff4fe656ccb84fe90a05385063d5e4c9a35
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-09-15 21:03:39 +0100

    edit route permission

--- SERVER/routes/others.js
+++ SERVER/routes/others.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.use(bodyParser.json());
 
 router.get('/:email/accounts',
-  Authenticate.verifyStaff,
+  Authenticate.verifyClient,
   AccountController.getAllAccountsOwnedByUser);
 
 // Admin create user
